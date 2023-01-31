614f2b5cbaddc8136afb59e047ee871449153e59
belkhira <BELKHIRA Mohamed Ali>
2023-01-31 14:19:16 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+import Home from './Home';
+import About from './About';
+
+
+function App() {
+  return (
+    
+      <div>
+         
+      <h1>Home Page</h1>
+  
+      <h2>islam&abdou&dali</h2>
+      <h3>TrendSetters</h3>
+        
+      </div>
+   
+  );
+}
+
+export default App;
